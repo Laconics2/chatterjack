35017b4be61e358dd3195a1f340f80b98e4c8e2c
gbp25 <92097493+gbp25@users.noreply.github.com>
2022-04-11 02:46:26 -0700

    Add files via upload

---
+++ coreEngine/message_generator_variation.py
@@ -0,0 +1,116 @@
+"""
+ This file is used for generating the answer according to the database and question.
+"""
+
+from coreEngine.segmentation import Segment
+from coreEngine.search import Search
+import random
+
+grab = Segment()
+keywordsAnswer = Search()
+
+class MessageGenerator():
+    def generatePronouns(self):
+        # this assumes the format of the pronouns section is "he/him/his"
+        if keywords["sex"] is not "TBA":
+            return keywords["sex"].split("/")
+        return "They", "Them", "Their"
+    def generateTimeString(self, whenString):
+        # assumes when string is in the format "MoWeFr 11:10AM - 12:20PM", if the populateDatabase script is used, this
+        # will be the format
+        possibleStartPhrase = {"This class is held on ",
+                              f"{keywords["class_name"]} has these meeting times:",
+                              "It's meeting times are"}
+        if whenString == "TBA":
+            return "I'm sorry, it appears there is no information about meeting times for this class."
+        else:
+            days, times = whenString.split(" ", 1)
+            if "Mo" in days:
+                days = days.replace("Mo", "Monday")
+            if "Tu" in days:
+                days = days.replace ("Tu", "Tuesday")
+            if "We" in days:
+                days = days.replace("We", "Wednesday ")
+            if "Th" in days:
+                days = days.replace("Th", "Thursday")
+            if "Fr" in days:
+               days = days.replace("Fr", "Friday")
+
+            startTime, endTime = times.split("-")
+            timePart = {f"{days} from {startTime} to {endTime}",
+                        f"{days} starting at {startTime}",
+                        f"{days} starting at {startTime} and ending at {endTime}"}
+
+            return possibleStartPhrase[random.randint(0,2)] + timePart[random.randint(0,2)]
+
+
+    def generate_answer(self, questions):
+        global keywords
+
+
+        table = keywordsAnswer.table(questions)
+        keywords = keywordsAnswer.searchKeyInf(questions)[0]
+
+
+        # generator the answer according pronouns
+        if table == 'PERSON':
+
+            # randomly select the person's name, spe_name, or pronouns
+            nameType = random.randint(0, 2)
+            if nameType:
+                speOrFullName = random.randint(0, 2)
+                if speOrFullName:
+                    if "person_name" in keywords.keys():
+                        personName = keywords["person_name"]
+                    else:
+                        personName = keywords['person_spe_name']
+                if '_where' in keywords.keys():
+                    answer = personName+"'s office is located in  " + keywords['_where'] + '.'
+                if '_who' in keywords.keys():
+                    answer = personName + " is the " + keywords['_who'] + '.'
+                if '_when' in keywords.keys():
+                    answer =  personName + " office hour is at " + keywords['_when'] + '.'
+
+            else:
+                pronouns = self.generatePronouns()
+
+                if '_where' in keywords.keys():
+                    answer = pronouns[2]+" office is located in  " + keywords['_where'] + '.'
+                if '_who' in keywords.keys():
+                    if pronouns[0].lower() is not "They":
+                        answer = pronouns[0]+ " is the " + keywords['_who'] + '.'
+                    else:
+                        answer = pronouns[0] + " are the " + keywords['_who'] + '.'
+                if '_when' in keywords.keys():
+                answer =  pronouns[2] + " office hour is at " + keywords['_when'] + '.'
+
+            return answer
+
+        if table == 'CLASS':
+            if '_where' in keywords.keys():
+                answer = "It is at " + keywords['_where'] + '.'
+            if '_who' in keywords.keys():
+                answer = keywords['_who'] + ' is the primary instructor.'
+            if '_when' in keywords.keys():
+                answer = self.generateTimeString(keywords["_when"])
+            if '_what' in keywords.keys():
+                answer = "It is about the knowledge of " + keywords['_what'] + '.'
+            return answer
+
+        if table == 'ORG':
+            if '_where' in keywords.keys():
+                answer = "It is at " + keywords['_where'] + '.'
+            if '_who' in keywords.keys():
+                answer = "Its dean is " + keywords['_who'] + '.'
+            if '_how' in keywords.keys():
+                answer = "You can take " + keywords['_how'] + ' there.'
+            if '_what' in keywords.keys():
+                answer = "It is " + keywords['_what'] + '.'
+            return answer
+
+
+"""
+answer = MessageGenerator()
+questions = input("Question: ")
+print(answer.generate_answer(questions))
+"""
