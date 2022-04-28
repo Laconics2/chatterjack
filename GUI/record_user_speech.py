003308238257465a76c36f416c76d3845735c0d6
taz36 <92078332+taz36@users.noreply.github.com>
2022-04-27 19:33:27 -0700

    Update record_user_speech.py

--- GUI/record_user_speech.py
+++ GUI/record_user_speech.py
@@ -1,12 +1,15 @@
 import sounddevice as sd
+from scipy.io.wavfile import write
 import wavio as wv
 
 
 def get_user_speech():
-    freq = 48000
-    duration = 10
-
-    recording = sd.rec(int(duration * freq), samplerate=freq, channels=2)
+    fs = 44100
+    duration = 6  # seconds
+    sd.default.samplerate = fs
+    sd.default.channels = 2
+    user_speech = sd.rec(int(duration * fs), samplerate=fs, channels=2)
     sd.wait()
-
-    wv.write("user_speech.wav", recording, freq, sampwidth=2)
+    write("user_speech_array.wav", fs, user_speech)
+    # Convert the NumPy array to audio file
+    wv.write("user_speech.wav", user_speech, fs, sampwidth=2)
